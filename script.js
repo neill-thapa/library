6077360bef34618065f635c4b0778b8a7a7bf8e2
Neil Thapa <thapaa.neill@gmail.com>
2026-04-06 17:08:50 +0000

    feat: Add initial html structure and logic
    - Added simple form
    - Added a logic for adding a book and displaying it to the screen

---
+++ script.js
@@ -0,0 +1,56 @@
+const bookForm = document.querySelector("#bookDetails")
+const display = document.querySelector("#display-books")
+
+const myLibrary = []
+
+bookForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const formData = new FormData(bookForm);
+
+    const newBook = new Book(
+        formData.get("title"),
+        formData.get("author"),
+        formData.get("pages"),
+        formData.get("bookDescription"),
+        formData.get("status")
+    )
+
+    addBookToLibrary(newBook)
+    displayBooks()
+
+    bookForm.reset()
+})
+
+class Book {
+    constructor(title, author, pages, about, status) {
+        this.id = crypto.randomUUID()
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.about = about
+        this.status = status
+    }
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book)
+}
+
+function displayBooks() {
+    display.innerHTML = ""
+
+    myLibrary.forEach(book => {
+        const bookCard = document.createElement("div")
+
+        bookCard.innerHTML = `
+        <h3>${book.title}</h3>
+        <p>Author: ${book.author}</p>
+        <p>Pages: ${book.pages}</p>
+        <p>Description: ${book.about}</p>
+        <p>Status: ${book.status}</p>
+        `
+
+        display.appendChild(bookCard)
+    })
+}
